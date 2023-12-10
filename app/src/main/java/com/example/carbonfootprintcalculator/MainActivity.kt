5880573e3bcfcf70a914b4d2ef9556eb837f8dcb
AhmedEhab125 <121432823+AhmedEhab125@users.noreply.github.com>
2023-12-10 02:27:36 +0200

    fix seekbar bug

--- app/src/main/java/com/example/carbonfootprintcalculator/MainActivity.kt
+++ app/src/main/java/com/example/carbonfootprintcalculator/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.carbonfootprintcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
 import androidx.lifecycle.ViewModelProvider
 import com.example.carbonfootprintcalculator.databinding.ActivityMainBinding
 import com.example.carbonfootprintcalculator.viewModel.CalculatorViewModel
@@ -16,13 +17,25 @@ class MainActivity : AppCompatActivity() {
         calculatorViewModel = ViewModelProvider(
             this
         ).get(CalculatorViewModel::class.java)
+       binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                binding.tvNumOfPepole.text = p1.toString()
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+            }
+        })
         binding.btnCalculate.setOnClickListener {
             binding.tvCarbonFoot.text = calculatorViewModel.calculateFootPrint(
                 energyConsumption = binding.edEnerge.text.toString(),
                 transportation = binding.etTransportation.text.toString(),
                 wastGeneration = binding.etWastGeneration.text.toString(),
                 waterUsage =  binding.etWaterUsage.text.toString(),
-                telecommunication =  binding.etTelecomunication.text.toString()
+                telecommunication =  binding.etTelecomunication.text.toString(),
+                numberOfPeople = binding.tvNumOfPepole.text.toString()
             )
         }
     }
